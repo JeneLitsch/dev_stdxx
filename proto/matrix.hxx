383564c96f4f906ef139af96597e4b4621f83280
Jene Litsch <litsch.jene@gmail.com>
2022-11-16 12:57:28 +0100

    stx::matrix ctor

---
+++ proto/matrix.hxx
@@ -0,0 +1,23 @@
+#pragma once
+#include <array>
+
+namespace stx {
+	template<typename T, std::size_t N, std::size_t M>
+	class matrix {
+	public:
+		matrix(T x = T{}) {
+			for(std::size_t i = 0; i < N * M; ++i) {
+				this->elements[i] = x;
+			}
+		}
+
+		matrix(T x, auto &&... args) 
+		: elements {x, static_cast<T>(args)...} {}
+
+		T & operator()(std::size_t n, std::size_t m) {
+			return this->elements[m * N + n];
+		}
+	private:
+		std::array<T, N * M> elements;
+	};
+}
--- sandbox/main.cxx
+++ sandbox/main.cxx
@@ -1,4 +1,17 @@
 #include <vector>
 #include <iostream>
+#include "proto/matrix.hxx"
 
-int main() {}
+int main() {
+	stx::matrix<float, 3, 4> m { 
+		1,  2,  3,
+		4,  5,  6,
+		7,  8,  9,
+		10, 11, 12,
+	};
+
+	stx::matrix<float, 3, 4> m2 { 42 };
+
+	std::cout << m(2, 3) << "\n";
+	std::cout << m2(2, 3) << "\n";
+}
