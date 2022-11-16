3b39cc388721b905d411c09ff2bb537bd2ec8f9d
Jene Litsch <litsch.jene@gmail.com>
2022-11-16 12:36:12 +0100

    Removed stx::vectorN prototype

--- sandbox/main.cxx
+++ sandbox/main.cxx
@@ -1,14 +1,4 @@
 #include <vector>
 #include <iostream>
-#include "proto/vectorN.hxx"
 
-int main() {
-	stx::vectorN<float, 5> vec1 = {1,2,3,4,5};	
-	stx::vectorN<float, 5> vec2 = {2,2,2,2,2};	
-	std::cout << "vec1: " << vec1 << "\n";
-	std::cout << "vec2: " << vec2 << "\n";
-	std::cout << "+:" << vec1 + vec2 << "\n";
-	std::cout << "-:" << vec1 - vec2 << "\n";
-	std::cout << "*:" << vec1 * vec2 << "\n";
-	std::cout << "/:" << vec1 / vec2 << "\n";
-}
+int main() {}
