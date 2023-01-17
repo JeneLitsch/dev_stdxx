2362352f2da6496a0d4a9bdaeac8c101226de561
Jene Litsch <litsch.jene@gmail.com>
2023-01-17 13:56:54 +0100

    Basic prototype for stx::graph

--- proto/graph.hxx
+++ proto/graph.hxx
@@ -1,6 +1,10 @@
 #pragma once
 #include <unordered_map>
 #include <set>
+#include <optional>
+#include <vector>
+#include <cstdint>
+#include <iterator>
 
 namespace stx {
 	class adjacency_list {
@@ -23,5 +27,139 @@ namespace stx {
 		}
 	private:
 		std::unordered_map<std::size_t, std::set<std::size_t>> table;
+	};
+
+
+
+	template<typename Node, typename AdjacencyDescriptor = adjacency_list>
+	class graph {
+		template<typename NodeT, typename GraphT>
+		struct basic_iterator {
+		public:
+			using iterator_category = std::input_iterator_tag;
+			using value_type = NodeT;
+			using pointer = value_type *;
+			using reference = value_type &;
+			using difference_type = std::uint64_t;
+
+			basic_iterator(GraphT * graph, std::size_t index)
+				: graph{graph}
+				, index{index} {}
+
+			NodeT & operator*() const {
+				return graph->nodes[index];
+			}
+
+			NodeT * operator->() const {
+				return &graph->nodes[index];
+			}
+
+			friend bool operator==(
+				const basic_iterator & l,
+				const basic_iterator & r) {
+				
+				if(l.graph != r.graph) return false;
+				else return l.index == r.index;
+			}
+
+			basic_iterator & operator++() {
+				this->index++;
+				return *this;
+			}
+
+			basic_iterator & operator--() {
+				this->index--;
+				return *this;
+			}
+
+			basic_iterator & operator++(int) {
+				auto old = *this;
+				(*this)++;
+				return old;
+			}
+
+			basic_iterator & operator--(int) {
+				auto old = *this;
+				(*this)--;
+				return old;
+			}
+
+		private:
+			std::size_t index;
+			GraphT * graph;
+		};
+
+	public:
+
+		using iterator = basic_iterator<Node, graph<Node>>;
+		using const_iterator = basic_iterator<const Node, const graph<Node>>;
+		
+
+
+		iterator insert(Node && node) {
+			const auto index = nodes.size(); 
+			nodes.push_back(std::move(node));
+			return iterator{this, index};
+		}
+
+
+
+		iterator insert(const Node & node) {
+			return this->insert(Node{node});
+		}
+
+
+
+		std::size_t size() const {
+			return std::size(this->nodes);
+		}
+
+
+
+		bool empty() const {
+			return std::empty(this->nodes);
+		}
+
+
+
+		const_iterator find(const Node & node) const {
+			auto index = find_impl(node);
+			return index ? const_iterator{this, *index} : this->end(); 
+		}
+
+
+
+		iterator find(const Node & node) {
+			auto index = find_impl(node);
+			return index ? iterator{this, *index} : this->end(); 
+		}
+
+
+		iterator begin() {
+			return {this, 0};
+		}
+
+		iterator end() {
+			return {this, this->size()};
+		}
+
+		const_iterator begin() const {
+			return {this, 0};
+		}
+
+		const_iterator end() const {
+			return {this, this->size()};
+		}
+
+	private:
+		std::optional<std::size_t> find_impl(const Node & node) const {
+			auto begin = std::begin(this->nodes);
+			auto end = std::end(this->nodes);
+			auto it = std::find(begin, end, node);
+			if(it == end) return std::nullopt;
+			else return static_cast<std::size_t>(std::distance(begin, it));
+		}
+		std::vector<Node> nodes;
+		AdjacencyDescriptor adj;	
 	};
 }
--- test/graph.cxx
+++ test/graph.cxx
@@ -1,2 +1,113 @@
 #include "proto/graph.hxx"
 #include "cphinx/cphinx.hxx"
+
+struct Node {
+	int data;
+	bool operator==(const Node &) const = default;
+	bool operator<=>(const Node &) const = default;
+};
+
+
+
+CPHINX_TEST(graph_empty) {
+	stx::graph<Node> graph;
+	cphinx.assert_equal(graph.size(), 0);
+	cphinx.assert_equal(std::size(graph), 0);
+	cphinx.assert_true(graph.empty());
+	cphinx.assert_true(std::empty(graph));
+}
+
+
+
+CPHINX_TEST(graph_insert_r_value) {
+	stx::graph<Node> graph;
+	auto it_a = graph.insert(Node{1});
+	auto it_b = graph.insert(Node{2});
+
+	cphinx.assert_equal(graph.size(), 2);
+	cphinx.assert_equal(std::size(graph), 2);
+	cphinx.assert_false(graph.empty());
+	cphinx.assert_false(std::empty(graph));
+
+	cphinx.assert_equal(it_a->data, 1);
+	cphinx.assert_equal(it_b->data, 2);
+}
+
+
+
+CPHINX_TEST(graph_insert_l_value) {
+	stx::graph<Node> graph;
+	Node a{1};
+	Node b{2};
+	auto it_a = graph.insert(a);
+	auto it_b = graph.insert(b);
+
+	cphinx.assert_equal(graph.size(), 2);
+	cphinx.assert_equal(std::size(graph), 2);
+	cphinx.assert_false(graph.empty());
+	cphinx.assert_false(std::empty(graph));
+
+	cphinx.assert_equal(it_a->data, 1);
+	cphinx.assert_equal(it_b->data, 2);
+}
+
+
+
+CPHINX_TEST(graph_find) {
+	stx::graph<Node> graph;
+	graph.insert(Node{42});
+	cphinx.assert_true(graph.find(Node{42}) != std::end(graph));
+	cphinx.assert_true(graph.find(Node{43}) == std::end(graph));
+	cphinx.assert_equal(graph.find(Node{42})->data, 42);
+}
+
+
+
+CPHINX_TEST(graph_find_const) {
+	stx::graph<Node> graph;
+	const auto & graph_ref = graph;
+	graph.insert(Node{42});
+	cphinx.assert_true(graph_ref.find(Node{42}) != std::end(graph_ref));
+	cphinx.assert_true(graph_ref.find(Node{43}) == std::end(graph_ref));
+	cphinx.assert_equal(graph_ref.find(Node{42})->data, 42);
+}
+
+
+
+CPHINX_TEST(graph_range_empty) {
+	stx::graph<Node> graph;
+	auto begin = std::begin(graph);
+	auto end = std::end(graph);
+	cphinx.assert_true(begin == end);
+	cphinx.assert_equal(std::distance(begin, end), 0);
+}
+
+
+
+CPHINX_TEST(graph_for) {
+	stx::graph<Node> graph;
+	graph.insert(Node{1});
+	graph.insert(Node{2});
+	graph.insert(Node{3});
+
+	std::size_t counter = 0;
+	for(const auto & node : graph) {
+		++counter;
+	}
+	cphinx.assert_equal(counter, std::size(graph));
+}
+
+
+
+CPHINX_TEST(graph_for_const) {
+	stx::graph<Node> graph;
+	graph.insert(Node{1});
+	graph.insert(Node{2});
+	graph.insert(Node{3});
+
+	std::size_t counter = 0;
+	for(const auto & node : std::as_const(graph)) {
+		++counter;
+	}
+	cphinx.assert_equal(counter, std::size(graph));
+}
